4a411db26c665864ddb18dead7d8e317ab653008
fox-58 <sophie@foxy.net.au>
2025-10-18 18:41:59 +1100

    Showing colours

--- metropolis/metropolis.py
+++ metropolis/metropolis.py
@@ -8,6 +8,7 @@ class Graph():
     def __init__(self, size):
         self.adjMatrix = [[0]*size for i in range(size)]
         self.size = size
+        self.adjMatrix = np.array(self.adjMatrix)
 
     def add_edge(self, v1, v2):
         if self.adjMatrix[v1][v2] == 1:
@@ -31,9 +32,15 @@ class Graph():
     def neighbors(self, v):
         return [i for i in range(self.size) if self.adjMatrix[v][i] == 1]
 
+    # def check_num_conflicts(self, colouring):
+    #     for i in range(self.size):
+    #         for j in range(self.size):
+    #             for u in graph.neighbors(i)
+
 def metropolis(graph, k, num_steps, start_colouring):
     current = start_colouring.copy()
     samples = [current.copy()]
+    num_conflicts = []
 
     for _ in range(num_steps):
         v = np.random.randint(0, graph.size)
@@ -47,15 +54,36 @@ def metropolis(graph, k, num_steps, start_colouring):
                     break
             if valid:
                 current[v] = new_colour
+        # num_conflicts = check_num_conflicts(current)
+        # num_conflicts.append(num_conflicts)
         samples.append(current.copy())
     return samples
 
-g = Graph(12)
+g = Graph(5)
 for _ in range(random.randint(10, 20)):
-    v1, v2 = random.sample(range(12), 2)
+    v1, v2 = random.sample(range(5), 2)
     g.add_edge(v1, v2)
 
-k = 7
+k = 25
 num_steps = 1500
 start_colouring = np.random.randint(0, k, g.size)
+print(start_colouring)
 samples = metropolis(g, k, num_steps, start_colouring)
+
+G = nx.from_numpy_array(g.adjMatrix)
+
+# pos = nx.spring_layout(G)
+
+# fig, ax = plt.subplots(figsize=(10, 8))
+# nx.draw(G, node_color=samples[1353], cmap=plt.cm.Spectral)
+# print(samples[1499])
+nx.draw(G, node_color=samples[1499], cmap=plt.cm.Spectral)
+
+# # plt.plot(g)
+# def update(frame):
+#     ax.clear()
+#     current = samples[frame]
+
+# anim = animation.FuncAnimation(g, update, frames=1500, interval=50)
+
+plt.show()
