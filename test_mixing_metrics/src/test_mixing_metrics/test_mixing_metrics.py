53a3da39e6cc4213568af780bc21d064a8c7f2ff
jthro <jethro.rosette@outlook.com>
2025-11-14 15:20:47 +1100

    Everything makes sense now

--- test_mixing_metrics/src/test_mixing_metrics/test_mixing_metrics.py
+++ test_mixing_metrics/src/test_mixing_metrics/test_mixing_metrics.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def hash_colouring(colouring):
     return hash(tuple(colouring))
 
-def gen_histogram(colourings):
+def gen_histogram(graph, colourings):
     # index with hash, contains tuple of order, frequency
     ordered_hist = {}
     n_distinct_colourings = 0
@@ -16,30 +16,75 @@ def gen_histogram(colourings):
         if key in ordered_hist:
             ordered_hist[key][1] += 1
         else:
-            ordered_hist[key] = [n_distinct_colourings, 1]
+            ordered_hist[key] = [n_distinct_colourings, 1, verify_colouring(graph, colouring)]
             n_distinct_colourings += 1
 
     # turn it into an array
     result = [0] * n_distinct_colourings
     for _, colouring in ordered_hist.items():
-        result[colouring[0]] = colouring[1]
+        result[colouring[0]] = [colouring[1], colouring[2]]
 
     return result
 
+# old jethro was too clever by half (quite literally)
+# - "the matrix is diagonally symmetrical i just need to fill in the top diagonal"
+# idiot
+def fix_adjacency_matrix(adjacency):
+    for i,row in enumerate(adjacency):
+        for j,cell in enumerate(row):
+            adjacency[i][j] |= cell
+
+    return adjacency
+
+# 2521 reference
+# returns True if valid colouring
+def verify_colouring(graph, colouring):
+    stack = [0]
+    visited = [False] * len(graph)
+    while not len(stack) == 0:        
+        curr = stack.pop()
+        if visited[curr]:
+            continue
+        
+        visited[curr] = True
+
+        for neighbour, edge in enumerate(graph[curr]):
+            if not edge or neighbour == curr:
+                continue
+            
+            if colouring[curr] == colouring[neighbour]:
+                return False
+            
+            stack.append(neighbour)
+
+        return True
+    
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("filename")
     args = parser.parse_args()
 
     with jsonlines.open(args.filename, mode='r') as reader:
-        colourings = reader.read()['colourings:']
-        histogram = gen_histogram(colourings)
-        histogram = np.array(histogram)
+        trial = reader.read()
+        adjacency = trial['graph']
+        adjacency = fix_adjacency_matrix(adjacency)
+        
+        colourings = trial['colourings:']
+        histogram = gen_histogram(adjacency, colourings)
+        
+        x = np.arange(len(histogram))
 
-        histogram_grouped = histogram.reshape(-1, 18).sum(axis=1)
-        x_grouped = np.arange(len(histogram_grouped))
+        freq = [h[0] for h in histogram]
+        valid = [10 if h[1] else 0 for h in histogram]
+
+        for i,v in enumerate(valid):
+            if not v == 0:
+                print(i)
+                break
+        
         fig, ax = plt.subplots()
-        ax.bar(x_grouped, histogram_grouped, width=1, edgecolor="white", linewidth = 0.7)
+        ax.plot(x, freq)
+        ax.plot(x, valid, linestyle="--", color="orange")
         plt.show()
 
             
