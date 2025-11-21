d671095d4fc3bb6c4e4ed32de5888f2b1a538211
fox-58 <sophie@foxy.net.au>
2025-11-21 12:39:38 +1100

    Fix stupid

--- visualisation/algorithms/invalid_vertex_weighted.py
+++ visualisation/algorithms/invalid_vertex_weighted.py
@@ -39,6 +39,7 @@ def invalid_vertex_weighted(graph, k, num_steps, start_colouring):
 
         v = np.random.randint(0, graph.size)
 
+        best_colour = np.argmin(invalid_neutral_array)
         old_colour = current[v]
 
         # Update
