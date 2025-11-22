2bb57e0fcdbcfdc6d00a354d0e57bfc2b1baac7e
Jethro <56021544+jthro@users.noreply.github.com>
2025-11-22 21:09:35 +1100

    Add MVPSRF script (#10)

--- test_mixing_metrics/src/test_mixing_metrics/test_mixing_metrics.py
+++ test_mixing_metrics/src/test_mixing_metrics/test_mixing_metrics.py
@@ -1,15 +1,23 @@
 from argparse import ArgumentParser
 import jsonlines
 
-from .data_processing import process_file_earliest_mixing_time
+from .data_processing import process_file_earliest_mixing_time, process_file_mvprsf
 from .metrics import aggregate_metric, plot_metric
 
 def main():
     parser = ArgumentParser()
     parser.add_argument("filename")
+    parser.add_argument("-s", "--statistic", help="valid: mean time to first valid colouring; rhat: multivariate PSRF")
     args = parser.parse_args()
 
     reader = jsonlines.open(args.filename, mode="r")
-    results = process_file_earliest_mixing_time(reader)
-    stats = aggregate_metric(results, metric_key="mixing_time")
-    plot_metric(stats, metric_name="mixing_time")
+
+    if (args.statistic == "valid"):
+        results = process_file_earliest_mixing_time(reader)
+        stats = aggregate_metric(results, metric_key="mixing_time")
+        plot_metric(stats, metric_name="mixing_time")
+        
+    else:
+        results = process_file_mvprsf(reader)
+        stats = aggregate_metric(results, metric_key="r_hat")
+        plot_metric(stats, metric_name="r_hat")
