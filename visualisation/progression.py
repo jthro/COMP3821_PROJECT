db521fe1bfd3a4e17948a07940dc47dff2888342
fox-58 <sophie@foxy.net.au>
2025-11-22 14:34:39 +1100

    Add creation video

---
+++ visualisation/progression.py
@@ -0,0 +1,174 @@
+from manim import *
+
+TITLE_COLOR = GREEN_E
+SUBTITLE_COLOR = GREY_B
+ACCENT_COLOR = GREEN_C
+NODE_COLOR = GREY_E
+EDGE_COLOR = GREY_B
+
+class GraphCreation(Scene):
+    def construct(self):
+        colour_map = color_gradient([RED, BLUE], 2)
+
+        positions = {
+            0: UP,
+            1: RIGHT + DOWN,
+            2: LEFT + DOWN,
+        }
+
+        scale = 2
+    
+        nodes = {}
+        for i in range(3):
+            circle = Circle(radius=0.3, color=WHITE, fill_opacity=1)
+            circle.set_fill(colour_map[0])
+            circle.move_to(positions[i]).shift(LEFT * 5)
+            nodes[i] = circle
+
+        edge_pairs = [(0, 1), (0, 2)]
+        edges = []
+        for (i, j) in edge_pairs:
+            edge = Line(nodes[i].get_center(), nodes[j].get_center(), stroke_width=3, color=EDGE_COLOR)
+            edges.append(edge)
+
+        self.play(*[Create(edge) for edge in edges], *[FadeIn(node) for node in nodes.values()])
+        self.wait()
+
+        arrows = [Arrow(4 *LEFT, 2* UL), Arrow(4*LEFT, 2* LEFT), Arrow(4*LEFT, 2*DL), Arrow(DL* 0.1 + DOWN * 2, RIGHT*2), Arrow(LEFT * 0.1, RIGHT*2)]
+
+        labels_text = ["0.3", "0.3", "0.3", "0.5", "0.5"]
+
+        labels = []
+        for arrow, text in zip(arrows, labels_text):
+            label = Text(text, font_size=24)
+            label.move_to((arrow.get_start() + arrow.get_end()) / 2 + UP*0.5)
+
+            vec = arrow.get_end() - arrow.get_start()
+            angle = np.arctan2(vec[1], vec[0])
+            label.rotate(angle)
+
+
+            labels.append(label)
+
+
+        # VGroup(*arrows).set_x(0).arrange(buff=2)
+        self.play(GrowArrow(arrows[0]), *[FadeIn(labels[0])])
+        # self.play(*[GrowArrow(a) for a in arrows], )
+        
+        nodes = {}
+        for i in range(3):
+            circle = Circle(radius=0.3, color=WHITE, fill_opacity=1)
+
+            if (i == 0): 
+                circle.set_fill(colour_map[1])
+            else:
+                circle.set_fill(colour_map[0])
+                    
+            circle.move_to(positions[i]).shift(LEFT).shift(UP *3)
+            nodes[i] = circle
+
+        edge_pairs = [(0, 1), (0, 2)]
+        edges = []
+        for (i, j) in edge_pairs:
+            edge = Line(nodes[i].get_center(), nodes[j].get_center(), stroke_width=3, color=EDGE_COLOR)
+            edges.append(edge)
+
+        self.play(*[Create(edge) for edge in edges], *[FadeIn(node) for node in nodes.values()])
+        self.wait()
+
+        loop = CurvedArrow(
+            start_point=UP*3 + LEFT*0.5,
+            end_point=UP*3 + RIGHT*0.5,
+            angle=TAU/2,
+            tip_length=0.2,
+            stroke_width=3,
+        )
+
+        self.add(loop)
+
+        self.play(GrowArrow(arrows[1]), *[FadeIn(labels[1])])
+
+        nodes = {}
+        for i in range(3):
+            circle = Circle(radius=0.3, color=WHITE, fill_opacity=1)
+
+            if (i == 1): 
+                circle.set_fill(colour_map[1])
+            else:
+                circle.set_fill(colour_map[0])
+                    
+            circle.move_to(positions[i]).shift(LEFT)
+            nodes[i] = circle
+
+        edge_pairs = [(0, 1), (0, 2)]
+        edges = []
+        for (i, j) in edge_pairs:
+            edge = Line(nodes[i].get_center(), nodes[j].get_center(), stroke_width=3, color=EDGE_COLOR)
+            edges.append(edge)
+
+        self.play(*[Create(edge) for edge in edges], *[FadeIn(node) for node in nodes.values()])
+        self.wait()
+
+        self.play(GrowArrow(arrows[2]), *[FadeIn(labels[2])])
+
+
+
+        nodes = {}
+        for i in range(3):
+            circle = Circle(radius=0.3, color=WHITE, fill_opacity=1)
+
+            if (i == 2): 
+                circle.set_fill(colour_map[1])
+            else:
+                circle.set_fill(colour_map[0])
+                    
+            circle.move_to(positions[i]).shift(LEFT).shift(DOWN*3)
+            nodes[i] = circle
+
+        edge_pairs = [(0, 1), (0, 2)]
+        edges = []
+        for (i, j) in edge_pairs:
+            edge = Line(nodes[i].get_center(), nodes[j].get_center(), stroke_width=3, color=EDGE_COLOR)
+            edges.append(edge)
+
+        self.play(*[Create(edge) for edge in edges], *[FadeIn(node) for node in nodes.values()])
+        self.wait()
+
+        self.play(GrowArrow(arrows[3]), *[FadeIn(labels[3])], GrowArrow(arrows[4]), *[FadeIn(labels[4])])
+
+
+        nodes = {}
+        for i in range(3):
+            circle = Circle(radius=0.3, color=WHITE, fill_opacity=1)
+
+            if (i == 2 or i ==1): 
+                circle.set_fill(colour_map[1])
+            else:
+                circle.set_fill(colour_map[0])
+                    
+            circle.move_to(positions[i]).shift(RIGHT * 3)
+            nodes[i] = circle
+
+        edge_pairs = [(0, 1), (0, 2)]
+        edges = []
+        for (i, j) in edge_pairs:
+            edge = Line(nodes[i].get_center(), nodes[j].get_center(), stroke_width=3, color=EDGE_COLOR)
+            edges.append(edge)
+
+        self.play(*[Create(edge) for edge in edges], *[FadeIn(node) for node in nodes.values()])
+        
+        
+        loop2 = CurvedArrow(
+            start_point=RIGHT*4,
+            end_point=RIGHT*5,
+            angle=TAU/2,
+            tip_length=0.2,
+            stroke_width=3,
+        )
+
+        self.add(loop2)
+
+        self.wait()
+
+
+
